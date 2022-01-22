ce5b4a6138bed8db064f57bf9712e5d797381b69
dimon4ess <98212150+dimon4ess@users.noreply.github.com>
2022-01-22 13:15:03 +0300

    Update I7o6eda.py

--- I7o6eda.py
+++ I7o6eda.py
@@ -16,8 +16,7 @@ def qw(_phone):
         _phone = '7'+_phone[1:]
     if _phone[0] == '9': 
         _phone = '7'+_phone 
-_phone = phone 
-    _name = '1' 
+_phone = phone  
     for x in range(12): 
         _name = _name + random.choice(list('123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM')) 
         password = _name + random.choice(list('123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM')) 
